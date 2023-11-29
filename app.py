ec08bc917273a3b602b151b067e8081815e2a9c0
1Uz1Kk <123676951+1Uz1Kk@users.noreply.github.com>
2023-11-29 19:49:59 +0000

    second

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ if __name__ == "__main__":
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/books")
+@app.route("/catalog")
+@app.route("/books/index")
+def books():
+    return render_template("books/index.html")
+@app.route("/books/book<int:id>")
+def book(id):
+    return render_template(f"books/book{id}.html")
